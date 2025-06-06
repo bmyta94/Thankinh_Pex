6d886dae545322754a8e4e9fe4b107a42697d706
bmyta94 <bmyta94@gmail.com>
2025-06-06 23:53:09 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const ThankinhPexApp());
+}
+
+class ThankinhPexApp extends StatelessWidget {
+  const ThankinhPexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Thankinh PEX',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Theo dõi PEX')),
+      body: const Center(
+        child: Text('Ứng dụng theo dõi điều trị thay huyết tương'),
+      ),
+    );
+  }
+}
