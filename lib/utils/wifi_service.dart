be5e66339ea4e7067b6faa1a05329dacf3b83f02
bmyta94 <bmyta94@gmail.com>
2025-06-06 22:36:36 +0700

    Update wifi_service.dart

--- lib/utils/wifi_service.dart
+++ lib/utils/wifi_service.dart
@@ -4,43 +4,45 @@ class WifiService {
   final Strategy strategy = Strategy.P2P_CLUSTER;
 
   void startAdvertising(String userName, Function(String id, String data) onDataReceived) async {
-    await Nearby().startAdvertising(
+    bool advertisingStarted = await Nearby().startAdvertising(
       userName,
       strategy,
       onConnectionInitiated: (id, connectionInfo) {
-        Nearby().acceptConnection(
-          id,
-          (payload) {
-            if (payload.type == PayloadType.BYTES) {
-              final data = String.fromCharCodes(payload.bytes!);
-              onDataReceived(id, data);
-            }
-          },
-        );
+        Nearby().acceptConnection(id);
       },
       onConnectionResult: (id, status) {},
       onDisconnected: (id) {},
     );
+
+    if (advertisingStarted) {
+      Nearby().setOnPayloadReceivedListener((endpointId, payload) {
+        if (payload.type == PayloadType.BYTES) {
+          final data = String.fromCharCodes(payload.bytes!);
+          onDataReceived(endpointId, data);
+        }
+      });
+    }
   }
 
   void startDiscovery(Function(String id, String data) onDataReceived) async {
-    await Nearby().startDiscovery(
+    bool discoveryStarted = await Nearby().startDiscovery(
       "Receiver",
       strategy,
       onConnectionInitiated: (id, connectionInfo) {
-        Nearby().acceptConnection(
-          id,
-          (payload) {
-            if (payload.type == PayloadType.BYTES) {
-              final data = String.fromCharCodes(payload.bytes!);
-              onDataReceived(id, data);
-            }
-          },
-        );
+        Nearby().acceptConnection(id);
       },
       onConnectionResult: (id, status) {},
       onDisconnected: (id) {},
     );
+
+    if (discoveryStarted) {
+      Nearby().setOnPayloadReceivedListener((endpointId, payload) {
+        if (payload.type == PayloadType.BYTES) {
+          final data = String.fromCharCodes(payload.bytes!);
+          onDataReceived(endpointId, data);
+        }
+      });
+    }
   }
 
   void stopAll() {
