1d37908a14f06d446023997d4314be132eb1d7f2
bmyta94 <bmyta94@gmail.com>
2025-06-06 21:59:08 +0700

    Update wifi_service.dart

--- lib/utils/wifi_service.dart
+++ lib/utils/wifi_service.dart
@@ -3,41 +3,44 @@ import 'package:nearby_connections/nearby_connections.dart';
 class WifiService {
   final Strategy strategy = Strategy.P2P_CLUSTER;
 
-  void startAdvertising(String userName, Function(String id, String data) onDataReceived) {
-    Nearby().startAdvertising(
+  void startAdvertising(String userName, Function(String id, String data) onDataReceived) async {
+    bool advertisingStarted = await Nearby().startAdvertising(
       userName,
       strategy,
       onConnectionInitiated: (id, connectionInfo) {
-        Nearby().acceptConnection(id, onDataReceivedCallback(id, onDataReceived));
+        Nearby().acceptConnection(id, (payload) {
+          if (payload.type == PayloadType.BYTES) {
+            final data = String.fromCharCodes(payload.bytes!);
+            onDataReceived(id, data);
+          }
+        });
       },
       onConnectionResult: (id, status) {},
       onDisconnected: (id) {},
     );
+    print("Advertising: $advertisingStarted");
   }
 
-  void startDiscovery(Function(String id, String data) onDataReceived) {
-    Nearby().startDiscovery(
+  void startDiscovery(Function(String id, String data) onDataReceived) async {
+    bool discoveringStarted = await Nearby().startDiscovery(
       "Receiver",
       strategy,
       onConnectionInitiated: (id, connectionInfo) {
-        Nearby().acceptConnection(id, onDataReceivedCallback(id, onDataReceived));
+        Nearby().acceptConnection(id, (payload) {
+          if (payload.type == PayloadType.BYTES) {
+            final data = String.fromCharCodes(payload.bytes!);
+            onDataReceived(id, data);
+          }
+        });
       },
       onConnectionResult: (id, status) {},
       onDisconnected: (id) {},
     );
+    print("Discovery: $discoveringStarted");
   }
 
   void stopAll() {
     Nearby().stopAdvertising();
     Nearby().stopDiscovery();
-  }
-
-  PayloadCallback onDataReceivedCallback(String id, Function(String id, String data) handler) {
-    return (endpointId, payload) {
-      if (payload.type == PayloadType.BYTES) {
-        final data = String.fromCharCodes(payload.bytes!);
-        handler(endpointId, data);
-      }
-    };
   }
 }
